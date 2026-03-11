0cf1453702ccaeb7499a560ca691a93fdddcdec0
Shuvojit Tikader <shuvojittikader@gmail.com>
2026-03-11 12:48:24 +0600

    save layout changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <Providers>
-          <Navbar />
+          <Navbar /> 
           <main className="min-h-screen container mx-auto px-4 py-6">
             {children}
           </main>
